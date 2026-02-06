3a2b1f5260c192f548a73c437c124cdded1238cf
S11UB11AYAN <shubhayanbagchi30@gmail.com>
2026-02-06 22:27:13 +0530

    basic backend setup done moving to frontend

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { authUser } from "../middlewares/auth.middleware.js";
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", authUser, getUsersForSidebar);
 router.get("/:id", authUser, getMessages);
+
+router.post("/send/:id", authUser, sendMessage);
 
 export default router;
